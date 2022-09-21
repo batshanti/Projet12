99ff4a4fa1e4983dc832cb3eeffb9c2402ac03aa
batshanti <teddylouvet@yahoo.fr>
2022-09-21 22:20:13 +0200

    add admin user creation

--- epic_events/user/admin.py
+++ epic_events/user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'team')
+
+    fieldsets = (
+        (
+            'User information',
+            {'fields': (
+                'username',
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+                'team'
+            )}
+        ),
+    )
+    search_fields = ('email',)
+
+
+admin.site.register(User, UserAdmin)
--- epic_events/user/models.py
+++ epic_events/user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    TEAM = [
+        ('manager', 'manager'),
+        ('sales', 'sales'),
+        ('support', 'support')
+    ]
+
+    team = models.CharField(max_length=10, choices=TEAM)
